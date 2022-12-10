63c8512164c42f6ab54970df71ddd6d8e887b7fc
Mavenry <sundaybukola10@gmail.com>
2022-12-10 09:55:05 -0800

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- *  main - prints the numbers in bsase 10
+ *  main - prints all the combinations of a single digits
  *  Return: Always 0
  */
 int main(void)
 {
-	int num;
-	char la;
+	int n;
 
-	for (num = 0 < 10; num++;)
-		putchar((num % 10) + '0');
-
-	for (la = 'a'; la <= 'f'; la++)
-		putchar(la);
-
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(',');
+		}
 	putchar('\n');
-
 	return (0);
+}
 }
