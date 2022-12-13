7a74b7f5efb72419f11a263c04bdacaee20f8ab7
Mavenry <sundaybukola10@gmail.com>
2022-12-13 14:31:40 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs -Computer the absolute value of an integer
+ * @t: the integer to be computed
+ * Return: The absolute va;ue
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
