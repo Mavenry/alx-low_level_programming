f8206e8ab22ab7bc4f30b9965c38b84a4e2b5f8b
Mavenry <sundaybukola10@gmail.com>
2023-01-06 11:56:02 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argc: he number of command line arguments
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
