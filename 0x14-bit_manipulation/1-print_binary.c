de42aefcfb4e713f1adddc45187df473ac7e2aa7
Mavenry <sundaybukola10@gmail.com>
2023-07-07 15:58:13 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,22 +2,31 @@
 
 /**
  * print_binary - prints the binary representation
- * of an unsigned long integer.
- * @num: the unsigned long integer to be printed in binary.
+ * of a number.
+ * @n: unsigned long int.
  *
- * Return: no return value.
+ * Return: no return.
  */
-void print_binary(unsigned long int num)
+void print_binary(unsigned long int number)
 {
-	if (num > 0)
-	{
-		if (num > 1)
-			print_binary(num >> 1);
-		_putchar((num & 1) + '0');
-	}
-	else
+	unsigned long int n = number;
+	unsigned int binary[64];
+	unsigned int i = 0;
+
+	if (n == 0)
 	{
 		_putchar('0');
+		return;
 	}
+
+	while (n > 0)
+	{
+		binary[i] = n % 2;
+		n /= 2;
+		i++;
+	}
+
+	for (int j = i - 1; j >= 0; j--)
+		_putchar(binary[j] + '0');
 }
 
