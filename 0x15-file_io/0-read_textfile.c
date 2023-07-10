35466b29022a45ee67b555e652dd6e20762aec0c
Mavenry <sundaybukola10@gmail.com>
2023-07-10 14:42:34 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,39 +6,43 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - Reads a text file and prints its contents to the standard output
+ * @filename: The name of the file to be read
+ * @letters: The number of letters to read and print
  *
- * Return: actual number of letters it could read and print
+ * This function opens the specified text file and reads the specified number
+ * of letters from it. The content is then printed to the standard output.
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
+	int fx;
+	ssize_t lent, lenx;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fx = open(filename, O_RDONLY);
+	if (fx == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 	{
-		close(fd);
+		close(fx);
 		return (0);
 	}
-	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
+	lent = read(fx, buff, letters);
+	close(fx);
+	if (lent == -1)
 	{
-		free(buffer);
+		free(buff);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
+	lenw = write(STDOUT_FILENO, buff, lenr);
+	free(buff);
+	if (lent != lenx)
 		return (0);
-	return (lenw);
+	return (lenx);
 }
